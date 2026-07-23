5c5bdb230ddd847f6d50dcbf5baa8b27c661c810
aqilahakmaa <167269442+aqilahakmaa@users.noreply.github.com>
2026-07-23 08:56:41 +0700

    Add WhatsApp notification configuration to settings
    Added WhatsApp notification configuration settings.

--- config/settings.py
+++ config/settings.py
@@ -4,3 +4,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATABASE_PATH = os.path.join(BASE_DIR, "database", "database.db")
 SNAPSHOT_DIR = os.path.join(BASE_DIR, "database", "snapshots")
+
+# --- KONFIGURASI NOTIFIKASI WHATSAPP (FONNTE) ---
+FONNTE_TOKEN = "BtS3yg2Hm4qVY79uZiDy"
+ADMIN_WA_NUMBER = "081378946896"  
